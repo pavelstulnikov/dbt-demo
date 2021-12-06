fdd3c7601569dd0744add03614ec344e5bc39b03
pavel.stulnikov@servian.com <pavel.stulnikov@servian.com>
2021-12-06 22:37:13 +0000

    Added macros

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -6,7 +6,7 @@ with payments as (
         status,
 
         -- Amount is stored in cents, convert to dollars
-        amount / 100 as amount,
+        {{ cents_to_dollars('amount', 4) }} as amount,
         created as created_at
 
     from {{ source('stripe', 'payment') }}
